fbb5a8beadcb3c512da6e1daf9e9b037c0ffbee9
edinelsonslima <edinelsonlima86@gmail.com>
2024-06-06 19:50:41 -0300

    refactor: remove unused div - add props for control position content

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -3,13 +3,17 @@ import { ComponentProps, memo, useEffect, useState } from 'react';
 import useSafeElementPosition from '@/hooks/use.safe.element.position';
 import s from './index.module.css';
 
-type IPopoverProps = ComponentProps<'div'> & {
+type IPopoverProps = ComponentProps<'button'> & {
   timer?: number;
+  positionPreference?: 'horizontal' | 'vertical';
 };
 
-function Component({ timer = 2_000, children, ...props }: IPopoverProps) {
+function Component({ timer = 2_000, positionPreference, children, ...props }: IPopoverProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [contentRef] = useSafeElementPosition<HTMLSpanElement>();
+
+  const [containerRef, contentRef] = useSafeElementPosition<HTMLButtonElement>({
+    positionPreference: positionPreference ?? 'vertical',
+  });
 
   const handleShowPopover = () => {
     setIsOpen(true);
@@ -27,20 +31,20 @@ function Component({ timer = 2_000, children, ...props }: IPopoverProps) {
   }, [isOpen, timer]);
 
   return (
-    <div className={s['popover-button-container']} {...props}>
-      <button
-        type="button"
-        onMouseEnter={handleShowPopover}
-        onMouseLeave={handleHidePopover}
-        onClick={handleShowPopover}
-        tabIndex={-1}
-        aria-haspopup="true"
-        aria-expanded={isOpen}
-        aria-controls={contentRef.current?.id}
-      >
-        ?
-      </button>
-
+    <button
+      ref={containerRef}
+      type="button"
+      onMouseEnter={handleShowPopover}
+      onMouseLeave={handleHidePopover}
+      onClick={handleShowPopover}
+      className={s['popover-container']}
+      tabIndex={-1}
+      aria-haspopup="true"
+      aria-expanded={isOpen}
+      aria-controls={contentRef.current?.id}
+      {...props}
+    >
+      ?
       <span
         ref={contentRef}
         className={isOpen ? s['not-sr-only'] : s['sr-only']}
@@ -50,7 +54,7 @@ function Component({ timer = 2_000, children, ...props }: IPopoverProps) {
       >
         {children}
       </span>
-    </div>
+    </button>
   );
 }
 
