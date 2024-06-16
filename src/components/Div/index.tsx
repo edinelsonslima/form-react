c9fdc335de1c0d30d8b09e46839818ccf3ac246e
edinelsonslima <edinelsonlima86@gmail.com>
2024-06-16 17:56:38 -0300

    refactor: add prefix in ccs props styles

--- src/components/Div/index.tsx
+++ src/components/Div/index.tsx
@@ -1,9 +1,20 @@
-import kebabCaseToCamelCase from '@/helpers/css.utilities';
 import { CSSProperties } from '@/types/css.properties';
-import { PropsWithChildren } from 'react';
+import { ComponentProps, PropsWithChildren } from 'react';
 
-function Div({ children, ...styles }: PropsWithChildren<CSSProperties>) {
-  return <div style={kebabCaseToCamelCase(styles)}>{children}</div>;
+function Div({ children, ...rest }: PropsWithChildren<CSSProperties & ComponentProps<'div'>>) {
+  const [props, styles] = Object.entries(rest).reduce<Record<string, string>[]>(
+    ([p, s], [key, value]) =>
+      !key.startsWith('__')
+        ? [{ ...p, [key]: value }, s]
+        : [p, { ...s, [key.replace(/^__/, '')]: value }],
+    [],
+  );
+
+  return (
+    <div style={styles} {...props}>
+      {children}
+    </div>
+  );
 }
 
 export default Div;
--- src/types/css.properties.ts
+++ src/types/css.properties.ts
@@ -1,11 +1,3 @@
 import { CSSProperties as CSS } from 'react';
 
-type KebabCase<T extends string> = T extends `${infer First}${infer Rest}`
-  ? `${First extends Uppercase<First> ? `-${Lowercase<First>}` : First}${KebabCase<Rest>}`
-  : T;
-
-type KebabCaseKeys<T extends object> = {
-  [k in keyof T as KebabCase<string & k>]: T[k];
-};
-
-export type CSSProperties = KebabCaseKeys<CSS>;
+export type CSSProperties = Partial<Record<`__${keyof CSS}`, CSS[keyof CSS]>>;
