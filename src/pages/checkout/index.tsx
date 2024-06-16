3403de993641303eab736cb5bd296db21f86d9a8
edinelsonslima <edinelsonlima86@gmail.com>
2024-06-16 17:59:57 -0300

    chore: remove initial values

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -7,46 +7,9 @@ import UserForm from './UserForm';
 
 import s from './index.module.css';
 
-type IForm = {
-  user: {
-    'user-name': string;
-    'user-email': string;
-    'user-ddi': number;
-    'user-phone': number;
-    'user-document': string;
-  };
-  credit: {
-    'holder-number': string;
-    'holder-validity': string;
-    'holder-cvv': string;
-    'holder-name': string;
-  };
-};
-
 function Checkout() {
-  const initialValues: IForm = {
-    user: {
-      'user-name': 'João da Silva',
-      'user-email': 'example@gmail.com',
-      'user-ddi': 55,
-      'user-phone': 11999999999,
-      'user-document': '123.456.789-00',
-    },
-    credit: {
-      'holder-number': '1234 5678 1234 5678',
-      'holder-validity': '12/23',
-      'holder-cvv': '123',
-      'holder-name': 'João da Silva',
-    },
-  };
-
   return (
-    <Form.Root<IForm>
-      id="main-form"
-      onSubmit={(data) => console.log(data)}
-      className={cn(s, 'checkout')}
-      initialValues={initialValues}
-    >
+    <Form.Root id="main-form" onSubmit={(data) => console.log(data)} className={cn(s, 'checkout')}>
       <UserForm />
 
       <Separate />
