801556004c98f15cc524c896a5027f77ff6be9e9
edinelsonslima <edinelsonlima86@gmail.com>
2024-06-17 12:24:10 -0300

    fix: update types of the props

--- src/components/Form/Inputs/Email/types.ts
+++ src/components/Form/Inputs/Email/types.ts
@@ -1,7 +1,10 @@
 import { IProps as IBaseProps } from '../Base/types';
+import { IProps as ISelectProps } from '../Select/types';
 
 type IControllerProps = IBaseProps;
 
-type IProps = IControllerProps;
+type IProps = IControllerProps & {
+  props: ISelectProps['props'];
+};
 
 export type { IControllerProps, IProps };
