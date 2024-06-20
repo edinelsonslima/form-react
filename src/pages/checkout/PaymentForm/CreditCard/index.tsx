8f901cdf0703098db921ced97e887f07b1077e76
edinelsonslima <edinelsonlima86@gmail.com>
2024-06-20 01:21:32 -0300

    feat: add prefix in credit number field

--- src/pages/checkout/PaymentForm/CreditCard/index.tsx
+++ src/pages/checkout/PaymentForm/CreditCard/index.tsx
@@ -6,6 +6,15 @@ function CreditCard() {
   return (
     <Form.Fieldset name="credit">
       <Form.Input
+        prefix={
+          <img
+            src="https://cdn.eduzzcdn.com/sun/assets/img/payment-method/brand/one_card.svg"
+            alt="Bandeira do cartão"
+            width={20}
+            height={20}
+            loading="eager"
+          />
+        }
         id="holder-number"
         name="holder-number"
         label="Número do Cartão"
