fea398e3d73542a1088796786480a39403297c70
edinelsonslima <edinelsonlima86@gmail.com>
2024-06-21 16:39:54 -0300

    refactor: improved readable

--- src/components/Form/Inputs/index.tsx
+++ src/components/Form/Inputs/index.tsx
@@ -7,34 +7,11 @@ type IProps = IPropsBase | ({ type: 'email' } & IPropsEmail);
 
 const Component = forwardRef<HTMLInputElement, IProps>(({ type, ...props }, ref) => {
   switch (type) {
-    case 'button':
-    case 'color':
-    case 'checkbox':
-    case 'date':
-    case 'file':
-    case 'datetime-local':
-    case 'hidden':
-    case 'image':
-    case 'month':
-    case 'number':
-    case 'password':
-    case 'radio':
-    case 'range':
-    case 'reset':
-    case 'search':
-    case 'submit':
-    case 'tel':
-    case 'time':
-    case 'url':
-    case 'week':
-    case 'text':
-      return <Input {...props} type={type} ref={ref} />;
-
     case 'email':
       return <InputEmail {...props} ref={ref} />;
 
     default:
-      return <Input {...props} ref={ref} />;
+      return <Input {...props} type={type} ref={ref} />;
   }
 });
 
