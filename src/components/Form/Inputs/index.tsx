f23c9c3134aaf4fb7d9e7129d212d165f7c281bd
Edinelson Lima <edinelsonlima86@gmail.com>
2024-06-28 14:03:22 -0300

    feat: add custom checkbox

--- src/components/Form/Inputs/index.tsx
+++ src/components/Form/Inputs/index.tsx
@@ -1,14 +1,21 @@
 import masks from '@/helpers/masks';
 import { forwardRef, memo } from 'react';
 import Input, { IProps as IPropsBase } from './Base';
-import InputEmail, { IProps as IPropsEmail } from './Email';
+import Checkbox, { IProps as IPropsCheckbox } from './Checkbox';
+import Email, { IProps as IPropsEmail } from './Email';
 
-type IProps = IPropsBase | ({ type: 'email' } & IPropsEmail);
+type IProps =
+  | IPropsBase
+  | ({ type: 'email' } & IPropsEmail)
+  | ({ type: 'checkbox' } & IPropsCheckbox);
 
 const Component = forwardRef<HTMLInputElement, IProps>(({ type, ...props }, ref) => {
   switch (type) {
     case 'email':
-      return <InputEmail {...props} ref={ref} />;
+      return <Email {...props} type={type} ref={ref} />;
+
+    case 'checkbox':
+      return <Checkbox {...props} type={type} ref={ref} />;
 
     default:
       return <Input {...props} type={type} ref={ref} />;
