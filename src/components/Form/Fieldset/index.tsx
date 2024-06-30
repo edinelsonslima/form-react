5ebabb93e92dd78f95a69b4ea5a22033c7f22669
Edinelson Lima <edinelsonlima86@gmail.com>
2024-06-30 15:35:46 -0300

    feat: add legend in fieldset

--- src/components/Form/Fieldset/index.tsx
+++ src/components/Form/Fieldset/index.tsx
@@ -1,15 +1,18 @@
 import cn from '@/helpers/cn';
-import { ComponentProps, PropsWithChildren, memo } from 'react';
+import { ComponentProps, PropsWithChildren, ReactNode, memo } from 'react';
 
 import s from './index.module.css';
 
 type IProps = ComponentProps<'fieldset'> & {
   name: string;
+  legend?: ReactNode;
+  layout?: 'column' | 'row';
 };
 
-function Component({ children, ...props }: PropsWithChildren<IProps>) {
+function Component({ children, legend, layout = 'column', ...props }: PropsWithChildren<IProps>) {
   return (
-    <fieldset className={cn(s, 'fieldset')} {...props}>
+    <fieldset className={cn(s, 'fieldset')} {...props} data-layout={layout}>
+      {legend && <legend className={cn(s, 'legend')}>{legend}</legend>}
       {children}
     </fieldset>
   );
