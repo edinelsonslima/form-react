91ace7f8a451fa4462d0973f985c73f18c4d5ba4
Edinelson Lima <edinelsonlima86@gmail.com>
2025-03-25 02:43:38 -0300

    chore: remove form test for mask

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { Checkout } from './pages/checkout';
-import { useMask, masks, Input } from './libs/mask';
 
 export function App() {
-  const inputRef = useMask(masks.currency());
-
   return (
     <>
       <h1
@@ -18,18 +15,6 @@ export function App() {
         Checkout
       </h1>
 
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData(e.currentTarget);
-          console.log(Object.fromEntries(formData.entries()));
-          console.log(inputRef.current?.['rb-value']);
-        }}
-      >
-        <input name="mask-ref" ref={inputRef} defaultValue={1.23} />
-        <Input name="mask-component" mask={masks.currency()} defaultValue={1.55} />
-        <input type="submit" value="enviar" />
-      </form>
       <Checkout />
     </>
   );
