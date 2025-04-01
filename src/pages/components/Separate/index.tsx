c0ffbb0c7f440be945324c1a3b6c117b04eb29b8
Edinelson Lima <edinelsonlima86@gmail.com>
2025-04-01 02:26:43 -0300

    fix: margin y separate

--- src/pages/components/Separate/index.tsx
+++ src/pages/components/Separate/index.tsx
@@ -1,13 +1,12 @@
 import { ComponentProps } from 'react';
 
 import s from './index.module.css';
+import { cn } from '@/libs/form/helpers/combine';
 
 type IProps = ComponentProps<'span'> & {
   transparent?: boolean;
 };
 
 export function Separate({ transparent, className, ...props }: IProps) {
-  return (
-    <span data-transparent={!!transparent} className={`${s.separate} ${className}`} {...props} />
-  );
+  return <span data-transparent={!!transparent} className={cn(s.separate, className)} {...props} />;
 }
